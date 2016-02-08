873d5042227cc0e5b4bf40d6dd08f7e43829c3c4
Mike Rodrigues <nismo9132@gmail.com>
2016-02-08 03:36:07 +0000

    Working version

---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var server = express();
+var dbConfig = require('../config.json');
+
+ 
+server.use(function(req, res, next) {
+    MongoClient.connect("mongodb://" + dbConfig.user + ":" + dbConfig.pass + "@ds059115.mongolab.com:59115/ab-test", function(err, db) {
+      if (err) {
+          return console.log('FAILURE CONNECTING TO DATABASE', req.url, req.headers['user-agent']);
+      }
+      console.log('connected');
+      // Use the data collection
+      var abCol = db.collection('data');
+      var data = {userAgent: req.headers['user-agent'], date: new Date()};
+      if (req.url.startsWith('/QR') || req.url.startsWith('/qr')) {
+          data.method = 'QR';
+          data.testCode = req.url.replace('/QR/', '').replace('/qr/', '');
+          res.send('Thanks for participating via QR code!');
+      } else if (req.url.startsWith('/URL') || req.url.startsWith('/url')) {
+          data.method = 'URL';
+          data.testCode = req.url.replace('/URL/', '').replace('/url/', '');
+          res.send('Thanks for participating via URL!');
+      } else {
+          console.log(req.url);
+          res.send('Hello!');
+      }
+      abCol.insert(data, function (err, result) {
+          if (err) {
+              return console.log('ERROR INSERTING', data);
+          } else {
+              console.log('SUCCESS');
+          }
+          db.close();
+      });
+      next();
+    });
+});
+ 
+var port = process.env.PORT;
+server.listen(port, function() {
+    console.log('server listening on port ' + port);
+});
